ad61536f02f348978ddbaa8c0567db2897704106
Uszczi <mateuszpapuga24@gmail.com>
2022-07-05 20:34:30 +0200

    add static movies

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 /** @jsx h */
+import { movies } from "../data/movies.ts";
 import Counter from "../islands/Counter.tsx";
 import { tw } from "@twind";
 import { h } from "preact";
@@ -6,17 +7,10 @@ import { h } from "preact";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <ul>
+        {movies.map((i) => <li>{i}</li>)}
+      </ul>
       <p>update</p>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
     </div>
   );
 }
