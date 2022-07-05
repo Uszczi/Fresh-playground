a79dc0abfc7b8e4e01a834da77a8ec439509ae2b
Uszczi <mateuszpapuga24@gmail.com>
2022-07-05 20:12:47 +0200

    test deploy

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,25 @@
+=============
+
+WARNING: You are currently running a version of TypeScript which is not officially supported by typescript-estree.
+
+You may find that it works just fine, or you may not.
+
+SUPPORTED TYPESCRIPT VERSIONS: >=3.2.1 <3.6.0
+
+YOUR TYPESCRIPT VERSION: 3.9.10
+
+Please only submit bug reports when using the officially supported version.
+
+=============
 /** @jsx h */
-import { h } from "preact";
-import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
+import { tw } from "@twind";
+import { h } from "preact";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <p>update</p>
       <img
         src="/logo.svg"
         height="100px"
