b47d64083a8d359253ff09d8f333435dd438c67a
Uszczi <mateuszpapuga24@gmail.com>
2022-07-05 20:53:54 +0200

    add working fzf

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,33 +1,65 @@
 /** @jsx h */
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import { movies } from "../data/movies.ts";
 import { tw } from "@twind";
+import { Fzf, FzfResultItem } from "https://esm.sh/fzf@0.5.1";
 import { h } from "preact";
 import { useState } from "preact/hooks";
 
-interface CounterProps {
-  start: number;
+interface HighlightProps {
+  str: string;
+  indices: Set<number>;
 }
+type DisplayList = FzfResultItem<string>[] | string[];
 
-export default function Counter(props: CounterProps) {
-  const [count, setCount] = useState(props.start);
-  const btn = tw`px-2 py-1 border(gray-100 1) hover:bg-gray-200`;
+const HighlightChars = (props: HighlightProps) => {
+  const chars = props.str.split("");
+
+  const nodes = chars.map((char, i) => {
+    if (props.indices.has(i)) {
+      return <b key={i}>{char}</b>;
+    } else {
+      return char;
+    }
+  });
+
+  return <p>{nodes}</p>;
+};
+
+export default function Counter() {
+  const fzf = new Fzf(movies, { limit: 22 });
+  const [display_list, setList] = useState<DisplayList>(movies);
+  const [st, setSt] = useState(false);
+
+  const onChange = (text) => {
+    console.log("adsfasdf");
+    const entries = fzf.find(text.target.value);
+    if (entries.length === 0) {
+      setList(entries);
+      setSt(false);
+    } else {
+      setList(entries);
+      setSt(true);
+    }
+  };
+
   return (
-    <div class={tw`flex gap-2 w-full`}>
-      <p class={tw`flex-grow-1 font-bold text-xl`}>{count}</p>
-      <button
-        class={btn}
-        onClick={() => setCount(count - 1)}
-        disabled={!IS_BROWSER}
-      >
-        -1
-      </button>
-      <button
-        class={btn}
-        onClick={() => setCount(count + 1)}
-        disabled={!IS_BROWSER}
-      >
-        +1
-      </button>
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <div>
+        <input onInput={onChange} />
+        <div>
+          {typeof display_list[0] !== "string"
+            ? display_list.map((entry, i) => (
+              <div key={i}>
+                <HighlightChars str={entry.item} indices={entry.positions} />
+              </div>
+            ))
+            : display_list.map((v, i) => (
+              <div key={i}>
+                <p>{v}</p>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -6,61 +6,10 @@ import { Fzf, FzfResultItem } from "https://esm.sh/fzf@0.5.1";
 import { h } from "preact";
 import { useState } from "preact/hooks";
 
-interface HighlightProps {
-  str: string;
-  indices: Set<number>;
-}
-type DisplayList = FzfResultItem<string>[] | string[];
-
-const HighlightChars = (props: HighlightProps) => {
-  const chars = props.str.split("");
-
-  const nodes = chars.map((char, i) => {
-    if (props.indices.has(i)) {
-      return <b key={i}>{char}</b>;
-    } else {
-      return char;
-    }
-  });
-
-  return <p>{nodes}</p>;
-};
-
 export default function Home() {
-  const fzf = new Fzf(movies, { limit: 22 });
-  const [display_list, setList] = useState<DisplayList>(movies);
-  const [st, setSt] = useState(false);
-
-  const onChange = (text) => {
-    console.log("adsfasdf");
-    const entries = fzf.find(text.target.value);
-    if (entries.length === 0) {
-      setList(items);
-      setSt(false);
-    } else {
-      setList(entries);
-      setSt(true);
-    }
-  };
-
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <div>
-        <input onInput={this.onChange} />
-        <div>
-          {typeof display_list[0] !== "string"
-            ? display_list.map((entry, i) => (
-              <div key={i}>
-                <HighlightChars str={entry.item} indices={entry.positions} />
-              </div>
-            ))
-            : display_list.map((v, i) => (
-              <div key={i}>
-                <p>{v}</p>
-              </div>
-            ))}
-        </div>
-      </div>
+      <Counter />
     </div>
   );
 }
